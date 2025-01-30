7c2c6daacc44a0aa330d76dcc15d5f981e2016d4
Mateus Alves <m.alvesealves@gmail.com>
2025-01-30 17:28:30 -0300

    creating-brand part 2

--- src/app/components/manage/brands/brands.component.ts
+++ src/app/components/manage/brands/brands.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { RouterLink } from '@angular/router';
+import { Brand } from 'src/app/interfaces/brand';
+import { BrandService } from 'src/app/services/brand.service';
 
 @Component({
   selector: 'app-brands',
@@ -19,15 +21,50 @@ import { RouterLink } from '@angular/router';
     MatSortModule,
     MatPaginatorModule,
     MatButtonModule,
-    RouterLink
+    RouterLink,
   ],
 })
-export class BrandsComponent {  
-  constructor() {    
+export class BrandsComponent {
+  displayedColumns: string[] = ['id', 'name', 'action'];
+  dataSource: MatTableDataSource<Brand>;
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  brandService = inject(BrandService);
+
+  constructor() {
+    this.dataSource = new MatTableDataSource([] as Brand[]);
   }
 
   ngOnInit() {
-    
+    this.getServerData();
+  }
+
+  private getServerData() {
+    this.brandService.getBrands().subscribe((result: any) => {
+      this.dataSource.data = result;
+    });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  delete(id: string) {
+    this.brandService.deleteBrandById(id).subscribe((res) => {
+      alert('Brand deleted');
+      this.getServerData();
+    });
   }
 }
